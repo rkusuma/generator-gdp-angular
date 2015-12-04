407ec0fc7cfc2764999e948676b86cd65d7396b0
Romy Kusuma <romy.kusuma@gdplabs.id>
2015-12-04 18:31:02 +0700

    Add dashboard route

---
+++ app/templates/src/client/app/dashboard/dashboard.route.js
@@ -0,0 +1,33 @@
+/**
+ * Created by romy.kusuma on 11/23/2015.
+ */
+(function() {
+    'use strict';
+
+    angular.module('app.dashboard')
+        .run(appRun);
+
+    function appRun(routerHelper) {
+        routerHelper.configureStates(getStates(), '/');
+    }
+
+    function getStates() {
+        return [
+            {
+                state: 'dashboard',
+                config: {
+                    url: '/',
+                    templateUrl: 'app/dashboard/dashboard.html',
+                    controller: 'DashboardController',
+                    controllerAs: 'vm',
+                    title: 'Dashboard',
+                    settings: {
+                        nav: 1,
+                        content: '<i class="fa fa-dashboard"></i> Dashboard'
+                    }
+                }
+            }
+        ];
+    }
+
+})();
