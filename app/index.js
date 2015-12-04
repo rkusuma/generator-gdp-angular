e80270c1a97ec95e7e9f4db3993b1da0da18e290
Romy Kusuma <romy.kusuma@gdplabs.id>
2015-12-04 15:02:56 +0700

    Add initial files
    1. Update gitignore
    2. Add index.js
    3. Add package.json

---
+++ app/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by romy.kusuma on 12/4/2015.
+ */
+'use strict';
+
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+    initializing: function() {
+
+    },
+    prompting: function() {
+
+    },
+    configuring: function() {
+
+    },
+    writing: function() {
+
+    },
+    install: function() {
+
+    }
+});
